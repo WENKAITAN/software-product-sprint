3d5c121e0f0d978a22eacde09ea3c9eb224d0af0
WenkaiTan <wenkaitan1019@gmail.com>
2021-06-02 21:26:58 +0000

    build portfolio web

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,6 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+function showImage(){
+    document.getElementById('loadingImage').style.visibility="visible";
+}
